ec0b8155db79b39c4fd7fdcb4a93fc6c6698cd36
Thomas Heijligen <thomas.heijligen@stud.tu-darmstadt.de>
2025-11-07 14:43:04 +0100

    test callibration class

--- src/tv_tuner/calibration.py
+++ src/tv_tuner/calibration.py
@@ -2,4 +2,12 @@
 import ROOT
 
 def test_calibration() -> None:
-    cal = ROOT.HDTV.Calibration();
+    print("  Testing class Calibraiton")
+    cal = ROOT.HDTV.Calibration()
+    cal1 = ROOT.HDTV.Calibration(1.0)
+    cal2 = ROOT.HDTV.Calibration(2.0, 3)
+    cal.SetCal(42.73)
+    print("  "
+        + str(cal1.Ch2E(5.2)) + " "
+        + str(cal2.dEdCh(5.2)) + " "
+        + str(cal.E2Ch(5.2)))
